9a30bb78824a55242205a0a90887ef77593a9b0b
jobayercse71 <jobayercse71.com>
2022-04-27 20:29:49 +0600

    env

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require ('express');
-const res = require('express/lib/response');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 var cors = require('cors');
 const app =express();
 app.use(cors());
-const port = process.env.PORT || 5000;
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 
-// ecoshop
-// 3g55OEDu8wGbJgxJ
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ipq6z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
